636a9b1cf343e62e8df9b825a987c1fb90b95717
1360837 <woowoo4599@gmail.com>
2025-01-31 15:49:04 +0900

    create push_swap_utils.c

--- push_swap/stack.c
+++ push_swap/stack.c
@@ -6,7 +6,7 @@
 /*   By: jiwnam <jiwnam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 18:03:03 by jiwnam            #+#    #+#             */
-/*   Updated: 2025/01/31 15:01:03 by jiwnam           ###   ########.fr       */
+/*   Updated: 2025/01/31 15:47:46 by jiwnam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,18 @@ int	pop(t_stack *st)
 int	is_empty(t_stack *st)
 {
 	return (st->top == -1);
+}
+
+int	is_sorted(t_stack *a)
+{
+	int	idx;
+
+	idx = 0;
+	while (idx < a->top)
+	{
+		if (a->stack[idx] < a->stack[idx + 1])
+			return (0);
+		idx++;
+	}
+	return (1);
 }
