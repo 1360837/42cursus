3a44fcc0a1d1adb70f65a4514caa251f7e923855
1360837 <woowoo4599@gmail.com>
2025-01-31 15:48:32 +0900

    create push_swap_utils.c

---
+++ push_swap/push_swap_utils.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiwnam <jiwnam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 15:38:35 by jiwnam            #+#    #+#             */
+/*   Updated: 2025/01/31 15:44:30 by jiwnam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	print_error(void)
+{
+	write(2, "Error\n", 6);
+	exit(0);
+}
+
+void	ft_free(void **a1, void **a2)
+{
+	if (a1 && *a1)
+	{
+		free(*a1);
+		*a1 = NULL;
+	}
+	if (a1 && *a1)
+	{
+		free(*a2);
+		*a2 = NULL;
+	}
+}
+
+int	is_num(char *str)
+{
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return (1);
+}
