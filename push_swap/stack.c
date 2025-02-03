9479e66b892702299ff848608c726f720c06d524
1360837 <woowoo4599@gmail.com>
2025-02-03 17:25:47 +0900

    fix error

--- push_swap/stack.c
+++ push_swap/stack.c
@@ -6,7 +6,7 @@
 /*   By: jiwnam <jiwnam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 18:03:03 by jiwnam            #+#    #+#             */
-/*   Updated: 2025/02/02 20:36:02 by jiwnam           ###   ########.fr       */
+/*   Updated: 2025/02/03 15:41:59 by jiwnam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,14 +49,16 @@ int	is_empty(t_stack *st)
 	return (st->top == -1);
 }
 
-int	is_sorted(t_stack *a)
+int	is_sorted(t_stack *a, int ascending)
 {
 	int	idx;
 
 	idx = 0;
 	while (idx < a->top)
 	{
-		if (a->stack[idx] < a->stack[idx + 1])
+		if (ascending == 0 && a->stack[idx] < a->stack[idx + 1])
+			return (0);
+		else if(ascending == 1 && a->stack[idx] > a->stack[idx + 1])
 			return (0);
 		idx++;
 	}
